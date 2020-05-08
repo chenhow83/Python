a854ed1d3c2f98f8189056a1b3dca70e488b1880
Chen How Chow <chenhow83@gmail.com>
2020-05-08 13:27:16 +0800

    hello world

---
+++ CS50 Web Programming with Python and JavaScript/hello.py
@@ -0,0 +1,24 @@
+x = 0
+
+if x > 0:
+    print("x is positivie!")
+
+elif x < 0:
+    print("x is negative!")
+
+else:
+    print(f"x - {x} is zero!")
+
+print(f"number is {x}!")
+
+name = "Alice"
+
+coordinates = (10.0, 20.0)
+
+names = ["alice", "bob", "cat"]
+
+for i in range(5):
+    print(i)
+
+for name in names:
+    print(name + " is sohai!")
